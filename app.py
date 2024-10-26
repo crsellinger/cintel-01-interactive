5422310f20118552adecb5628929dee5fe16b996
Caleb Sellinger <calebsell24@gmail.com>
2024-10-25 23:23:16 -0500

    Update app.py
    Colors update. OoOoOo!

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-"""######################################
-Caleb Sellinger
-Module 1
-Dr. Case
-10-25-2024
-######################################"""
+#Caleb Sellinger
+#44630 Module 1
+#Dr. Case
+#10-25-2024
+
+"""
+Look at this Histogram! Wow!
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
 from shiny.express import ui, input, render
+from shinyswatch import theme
 
 #Page options, specifically the title
-ui.page_opts(title="Big TITLE, Very Shiny",fillable=True)
+ui.page_opts(title="Big TITLE, Very Shiny",theme = theme.quartz,fillable=True)
 
 #slider
 with ui.sidebar():
@@ -25,4 +28,4 @@ def histogram():
     np.random.seed(19680801)
     x = 100 + 15 * np.random.randn(437)
     #function to create plot
-    plt.hist(x, input.selected_number_of_bins(), density=True)
+    plt.hist(x, input.selected_number_of_bins(), color="cyan",density=True,edgecolor="blue")
